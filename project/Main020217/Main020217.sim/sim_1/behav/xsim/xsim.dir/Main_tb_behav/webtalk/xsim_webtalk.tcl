eadab7dd3fff08234a63d95068cd9ba9f25373e0
Melika Keshavarzmirzamohammadi <91981045+Melikakmm@users.noreply.github.com>
2023-05-14 19:03:40 +0200

    python bubble sort added

--- project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/xsim_webtalk.tcl
+++ project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sat May 13 21:00:57 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat May 13 21:14:21 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2021.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3247384" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "e56d428e-22a1-4d24-8ae4-bc6c32751a73" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "55be60ae05964843aa08c3249dbaff20" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "11" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 20.04.5 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8750H CPU @ 2.20GHz" -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "us
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "2101 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.35_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "124932_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1827540994 -regid "" -xml /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.02_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "120832_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 90759408 -regid "" -xml /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/melika/GitHub/Bubblesort_FPGA/project/Main020217/Main020217.sim/sim_1/behav/xsim/xsim.dir/Main_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
